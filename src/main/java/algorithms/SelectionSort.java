1945e592172c9c2f56984af959de4b09fbfbd1ab
Abdunur <amangeldievabdunur@gmail.com>
2025-10-04 00:27:01 +0600

    baseline selection sort implementation

---
+++ src/main/java/algorithms/SelectionSort.java
@@ -0,0 +1,49 @@
+package algorithms;
+
+import metrics.PerformanceTracker;
+
+
+public class SelectionSort {
+
+    private final PerformanceTracker tracker;
+
+    public SelectionSort(PerformanceTracker tracker) {
+        this.tracker = tracker;
+    }
+
+    public void sort(int[] arr) {
+        tracker.reset();
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                tracker.incrementComparisons();
+                tracker.incrementArrayAccesses();
+                tracker.incrementArrayAccesses();
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                swap(arr, i, minIndex);
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        tracker.incrementSwaps();
+        tracker.incrementArrayAccesses();
+        tracker.incrementArrayAccesses();
+        tracker.incrementArrayAccesses();
+        tracker.incrementArrayAccesses();
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
